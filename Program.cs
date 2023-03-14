da172f954e9c117301557611492fd9d0a89269f3
Shinshi11a <pro100vanyok2016@gmail.com>
2023-03-14 19:36:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿public class SQLCommand
+{
+    private string commandText;
+
+    public SQLCommand(string commandText)
+    {
+        CommandText = commandText;
+    }
+
+    public string CommandText
+    {
+        get { return commandText; }
+        set { commandText = value.ToUpper(); }
+    }
+}
